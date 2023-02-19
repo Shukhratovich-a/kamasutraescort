9f419cb948b4dc37d5d3f52ee958499b67566a6f
shukhratovich-a <shukhratovich138@gmail.com>
2023-02-20 03:55:55 +0500

    Check token

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,15 +1,31 @@
+import React from "react";
+import { GetServerSideProps } from "next";
+import { useRouter } from "next/router";
+import { getSession } from "next-auth/react";
+import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 import { withProfileLayout } from "../../layout/Layout";
 
-const Profile = (): JSX.Element => {
+const Profile = ({ session }: { session: object }): JSX.Element => {
+  const router = useRouter();
+  const { i18n } = useTranslation();
+
+  React.useEffect(() => {
+    if (!session) {
+      router.push("/", "/", { locale: i18n.language });
+    }
+  });
+
   return <>dfsf</>;
 };
 
-export const getStaticProps = async ({ locale }: { locale: string }) => {
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const session = await getSession(ctx);
   return {
     props: {
-      ...(await serverSideTranslations(locale)),
+      session: session,
+      ...(await serverSideTranslations(String(ctx.locale))),
     },
   };
 };
